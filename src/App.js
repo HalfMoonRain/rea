067e9c77a0ce163c9937cd570d2b5b0ee42c9ff4
junwoo <junwoo1357@gmail.com>
2023-12-28 20:44:06 +0900

    make a board

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Hello, World
-        </p>
-      </header>
-    </div>
-  );
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
-export default App;
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
+}
